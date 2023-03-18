c8136ac4082e28572bfc4079746e718b89ce58b6
Elrojobarra30 <126982870+Elrojobarra30@users.noreply.github.com>
2023-03-18 19:53:21 -0300

    Curriculum online

---
+++ script/script.js
@@ -0,0 +1,12 @@
+/*window.alert("Bienvenidos A mi CV-online");*/
+
+/*function mostrarNombre(nombre) {
+    const nombreHeader = document.createElement("h3");
+    nombreHeader.textContent = nombre;
+    /*document.querySelector("header").appendChild(nombreHeader);
+    document.querySelector("footer").appendChild(nombreHeader);
+}
+
+
+mostrarNombre("Argentina Programa 4.0");*/
+
